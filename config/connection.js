fa77d0c4211e73120fd591c749b399d8a51af64b
Rachael Gladwin <rachaelgladwin@Rachaels-iMac.local>
2020-05-10 10:37:03 -0600

    updated jawsdb code in connections

--- config/connection.js
+++ config/connection.js
@@ -27,19 +27,5 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
 });
 
-// connection.query("USE bu09izwutcdhkvpy", function(err) {
-//     if (err) throw err;
-//     connection.query('CREATE TABLE IF NOT EXISTS burgers(' +
-//         'id INT NOT NULL AUTO_INCREMENT,' +
-//         'PRIMARY KEY(id),' +
-//         'link VARCHAR(255),' +
-//         'item VARCHAR(255),' +
-//         'stock VARCHAR(255)' +
-//         ')',
-//         function(err) {
-//             if (err) throw err;
-//         });
-// });
-
 // Export connection for our ORM to use.
 module.exports = connection;
